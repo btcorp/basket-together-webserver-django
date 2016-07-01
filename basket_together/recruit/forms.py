2e3911a85042bd60139ef13d7fd1e88878c6b660
Taewoo <qkqnrpa@gmail.com>
2016-07-01 16:54:33 +0900

    set summernote

--- basket_together/recruit/forms.py
+++ basket_together/recruit/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from recruit.models import Post, Comment
 from recruit.widgets import GoogleMapWidget
 from django_summernote.widgets import SummernoteWidget
+from crispy_forms.helper import FormHelper
 
 
 class PostForm(forms.ModelForm):
@@ -9,9 +10,16 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'content', 'recruit_count', 'latlng', )
         widgets = {
-            # 'content': SummernoteWidget,
+            'content': SummernoteWidget,
             'latlng': GoogleMapWidget,
         }
+
+    # def __init__(self, *args, **kwargs):
+    #     super(PostForm, self).__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.form_class = 'form-horizontal'
+    #     self.helper.label_class = 'col-lg-2'
+    #     self.helper.field_class = 'col-lg-10'
 
 
 class CommentForm(forms.ModelForm):
