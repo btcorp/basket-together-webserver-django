25ebb67177111ed8fcbfdc9e1554083cf618e6ed
Taewoo <qkqnrpa@gmail.com>
2016-07-06 14:40:36 +0900

    index page에서 meeting_date가 가장 가까운 posting 8개만 출력되도록 수정.

--- basket_together/basket_together/views.py
+++ basket_together/basket_together/views.py
@@ -3,6 +3,6 @@ from recruit.models import Post
 
 
 def index(request):
-    posts = Post.objects.all()
-    return render(request, 'recruit/index.html', {'posts':posts})
+    posts = Post.objects.order_by('-meeting_date')[:8]
+    return render(request, 'recruit/index.html', {'posts': posts})
 
