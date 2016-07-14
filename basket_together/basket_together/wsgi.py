afc7bf447d2c111ea6518f20987b6844ab4eef86
Taewoo <qkqnrpa@gmail.com>
2016-07-14 14:25:03 +0900

    change settings

--- basket_together/basket_together/wsgi.py
+++ basket_together/basket_together/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os, sys, site
 
 site.addsitedir("/Users/Jin-TaeWoo/.virtualenvs/projectenv/lib/python3.5/site-packages")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basket_together.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basket_together.settings.dev")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
