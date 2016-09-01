087c479c0076cf18030753ea223ffb6db2d58dab
Taewoo <qkqnrpa@gmail.com>
2016-09-01 18:02:51 +0900

    modify database settings

--- basket_together/settings/common.py
+++ basket_together/settings/common.py
@@ -122,14 +122,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': os.path.join(PROJECT_FOLDER, '../database/db_web.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db_web.sqlite3'),
         'USER': 'test',
         'PASSWORD': '1234qwer'
     },
     'api_server': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': os.path.join(PROJECT_FOLDER, '../database/db_api.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db_api.sqlite3'),
         'USER': 'test',
         'PASSWORD': '1234qwer'
     }
