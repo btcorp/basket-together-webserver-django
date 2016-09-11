9943fcbeb7158d35de2470495f0b5e653617341c
Taewoo <qkqnrpa@gmail.com>
2016-09-11 15:12:57 +0900

    auth.validator가 import 되지 않는 버그 수정.(원인은 django 1.9버전에서의 버그

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@
 import re
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin, User
-# from django.contrib.auth.validators import ASCIIUsernameValidator, UnicodeUsernameValidator
+from django.contrib.auth.validators import ASCIIUsernameValidator, UnicodeUsernameValidator
 from django.db import models
 from django.forms import ValidationError
 from django.utils import six, timezone
@@ -117,14 +117,14 @@ class Friendship(models.Model):
 
 
 class ExtendedUser(AbstractUser):
-    # username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
+    username_validator = UnicodeUsernameValidator() if six.PY3 else ASCIIUsernameValidator()
 
     username = models.CharField(
         _('Username or Email'),
         max_length=150,
         unique=True,
         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        # validators=[username_validator],
+        validators=[username_validator],
         error_messages={
             'unique': _("A user with that username already exists."),
         },
