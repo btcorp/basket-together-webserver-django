cc1a78cd2cfb98fb56007a753efaa88b06702ee9
Taewoo <qkqnrpa@gmail.com>
2016-10-06 13:45:40 +0900

    facebook secret_key 수정

--- basket_together/settings/dev.py
+++ basket_together/settings/dev.py
@@ -1,1 +1,6 @@
 from .common import *
+
+# Facebook App ID
+SOCIAL_AUTH_FACEBOOK_KEY = '1141181879288987'
+# Facebook App Secret Key
+SOCIAL_AUTH_FACEBOOK_SECRET = '4a9f148f3df1e68cb32654bfbe361c4c'
--- basket_together/settings/prod.py
+++ basket_together/settings/prod.py
@@ -11,12 +11,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 # Django 내 암호화 시에 salt 값으로 사용.
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ubuntu',
-        'USER': 'ubuntu',
-        'PASSWORD': '1234qwer',
-        'HOST': '127.0.0.1',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ubuntu',
+#         'USER': 'ubuntu',
+#         'PASSWORD': '1234qwer',
+#         'HOST': '127.0.0.1',
+#     }
+# }
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1141173975956444'
+SOCIAL_AUTH_FACEBOOK_SECRET = 'd829612eb38f7d98c0434e3379ee545a'
