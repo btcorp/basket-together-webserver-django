bd7d0726940aae4aaba9195e5dee832f01907720
Taewoo <qkqnrpa@gmail.com>
2016-11-16 22:44:44 +0900

    add google place api

--- recruit/widgets.py
+++ recruit/widgets.py
@@ -7,7 +7,7 @@ from django.template.loader import render_to_string
 
 class GoogleMapWidget(forms.HiddenInput):
     def render(self, name, value, attrs=None):
-        lat, lng = '37.497921', '127.027636'    # 강남역
+        lat, lng = '37.497921', '127.027636'    # default center of map
 
         if value:
             lat, lng = re.findall(r'[\d\.]+', value)
@@ -18,5 +18,4 @@ class GoogleMapWidget(forms.HiddenInput):
             'base_lng': lng,
         })
 
-        # return mark_safe(html)
         return super(GoogleMapWidget, self).render(name, value, attrs) + html
