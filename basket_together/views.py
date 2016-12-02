c2336946f2b6903bc3c0700d33d9c861bb2084df
Taewoo <qkqnrpa@gmail.com>
2016-12-02 19:31:26 +0900

    modify posting count on index

--- basket_together/views.py
+++ basket_together/views.py
@@ -3,6 +3,6 @@ from recruit.models import Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-meeting_date')[:8]
+    posts = Post.objects.order_by('-registered_date')[:12]
     return render(request, 'recruit/index.html', {'posts': posts})
 
