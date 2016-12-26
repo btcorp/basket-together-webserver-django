6ccb14206261853ad272285ab7885267aad0e06b
Taewoo <qkqnrpa@gmail.com>
2016-12-26 17:15:39 +0900

    수정된 css파일이 반영되지 않는 버그 수정

--- recruit/templatetags/secretkey_tags.py
+++ recruit/templatetags/secretkey_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from basket_together.settings import common
+import os
 
 secret_values = {
     'GOOGLE_MAP_API_KEY': common.GOOGLE_MAP_API_KEY,
@@ -13,3 +14,8 @@ register = template.Library()
 @register.simple_tag(name='SECRET_KEY')
 def get_secret_keys(key_name):
     return getattr(common, key_name)
+
+
+@register.simple_tag(name='FILE_M_TIME')
+def get_modify_file_time(file_name):
+    return os.path.getmtime(common.STATIC_ROOT+'/'+file_name)
